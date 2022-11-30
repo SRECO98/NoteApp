6f11dd26992fb287008a045e2ca9c6bbe090e481
Sreten <banovicbl22@gmail.com>
2022-11-30 18:07:38 +0100

    Adding Screens 1
    DefaultRadioButton, NoteItem, OrderSection and making special note with Canvas

--- app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/com/example/noteapp/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,6 @@
 package com.example.noteapp.feature_note.domain.use_case
 
-data class NoteUseCases( //this class will be ejected in viewmodel, because we will make out code cleaner if we call only one class and not every use case one by one.
+data class NoteUseCases( //this class will be ejected in viewmodel, because we will make our code cleaner if we call only one class and not every use case one by one.
     val getNotesUseCase: GetNotesUseCase,
     val deleteNoteUseCase: DeleteNoteUseCase,
     val addNoteUseCase: AddNoteUseCase,
