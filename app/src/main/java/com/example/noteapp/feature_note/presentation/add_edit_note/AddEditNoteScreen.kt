795d0faf32387ec47ff17a8fab5fc5038595a50f
Sreten <banovicbl22@gmail.com>
2022-12-01 21:15:33 +0100

    AddEditNoteScreen
    Scaffold, floatingButton, TransparentHintTextField

--- app/src/main/java/com/example/noteapp/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
+++ app/src/main/java/com/example/noteapp/feature_note/presentation/add_edit_note/AddEditNoteScreen.kt
@@ -1,8 +1,148 @@
 package com.example.noteapp.feature_note.presentation.add_edit_note
 
+import androidx.compose.animation.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Save
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.noteapp.feature_note.domain.model.Note
+import com.example.noteapp.feature_note.presentation.add_edit_note.components.TransparentHintTextField
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @Composable
-fun AddEditNoteScreen() {
+fun AddEditNoteScreen(
+    navController: NavController,
+    noteColor: Int,
+    viewModel: AddEditNoteViewModel = hiltViewModel()
+) {
+    val titleState = viewModel.noteTitle.value
+    val contantState = viewModel.noteContent.value
+    val scaffoldState = rememberScaffoldState() //to show Snackbars
+    val noteBackgroundAnimatable = remember {
+        Animatable(
+            Color(
+                if (noteColor != -1) {
+                    noteColor
+                } else {
+                    viewModel.noteColor.value
+                }
+            )
+        )
+    }
+    val scope = rememberCoroutineScope()
     
+
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    viewModel.onEvent(AddEditNoteEvent.SaveNote)
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Save,
+                    contentDescription = "Save note",
+                )
+            }
+        },
+        scaffoldState = scaffoldState,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(noteBackgroundAnimatable.value)
+                .padding(16.dp)
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Note.noteColors.forEach { color -> //making little circle for every color.
+                    val colorInt = color.toArgb()
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .shadow(15.dp, CircleShape)
+                            .clip(CircleShape)
+                            .background(color)
+                            .border(
+                                width = 3.dp,
+                                color = if (viewModel.noteColor.value == colorInt) { //if circle is selected its getting black border
+                                    Color.Black
+                                } else { // else its getting transparent border.
+                                    Color.Transparent
+                                },
+                                shape = CircleShape
+                            )
+                            .clickable { //if user clicks one of color circles ir will trigget this animation to change background color of screen.
+                                scope.launch {
+                                    noteBackgroundAnimatable.animateTo(
+                                        targetValue = Color(colorInt),
+                                        animationSpec = tween(
+                                            durationMillis = 500
+                                        ),
+                                    )
+                                }
+                                viewModel.onEvent(AddEditNoteEvent.ChangeColor(colorInt))
+                            }
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TransparentHintTextField(
+                text = titleState.text,
+                hint = titleState.hint,
+                onValueChange = {
+                                viewModel.onEvent(AddEditNoteEvent.EnteredTitle(it))
+                },
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeTitleFocus(it))
+                },
+                isHintVisible = titleState.isHintVisible,
+                singleLine = true,
+                textStyle = MaterialTheme.typography.h5,
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            TransparentHintTextField(
+                text = contantState.text,
+                hint = contantState.hint,
+                onValueChange = {
+                    viewModel.onEvent(AddEditNoteEvent.EnteredContent(it))
+                },
+                onFocusChange = {
+                    viewModel.onEvent(AddEditNoteEvent.ChangeContentFocus(it))
+                },
+                isHintVisible = contantState.isHintVisible,
+                textStyle = MaterialTheme.typography.body1,
+                modifier = Modifier.fillMaxHeight()
+            )
+        }
+    }
 }
