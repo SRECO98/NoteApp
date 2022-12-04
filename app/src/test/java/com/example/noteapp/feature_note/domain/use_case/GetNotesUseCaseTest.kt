e5d6effb9b15c682ef6e92431e45713b6b812594
Sreten <banovicbl22@gmail.com>
2022-12-04 17:02:36 +0100

    GetNotesUseCaseTest
    Added test for correct order by title, ascending. - Passed

--- app/src/test/java/com/example/noteapp/feature_note/domain/use_case/GetNotesUseCaseTest.kt
+++ app/src/test/java/com/example/noteapp/feature_note/domain/use_case/GetNotesUseCaseTest.kt
@@ -2,9 +2,13 @@ package com.example.noteapp.feature_note.domain.use_case
 
 import com.example.noteapp.feature_note.data.repository.FakeNoteRepository
 import com.example.noteapp.feature_note.domain.model.Note
+import com.example.noteapp.feature_note.domain.util.NoteOrder
+import com.example.noteapp.feature_note.domain.util.OrderType
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Test
 
 class GetNotesUseCaseTest{
     private lateinit var getNotesUseCase: GetNotesUseCase
@@ -34,5 +38,14 @@ class GetNotesUseCaseTest{
         }
     }
 
+    @Test
+    fun `Order notes by title ascending, correct order`() = runBlocking{
+        //we use first here cause its returning a flow and not a list, so we need only first emission
+        val notes = getNotesUseCase(NoteOrder.Title(OrderType.Ascending)).first() //getting list of notes
+
+        for(i in 0..notes.size - 2){
+            assertThat(notes[i].title).isLessThan(notes[i+1].title)
+        }
+    }
 
 }
