e793e8186688a08feb715bba97f58e1771c0e2ae
Sreten <banovicbl22@gmail.com>
2022-12-05 19:44:09 +0100

    NotesScreenTest
    Added 2 rules and @Before fun for testing.

--- app/src/androidTest/java/com/example/noteapp/feature_note/presentation/notes/NotesScreenTest.kt
+++ app/src/androidTest/java/com/example/noteapp/feature_note/presentation/notes/NotesScreenTest.kt
@@ -1,12 +1,50 @@
 package com.example.noteapp.feature_note.presentation.notes
 
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.noteapp.di.AppModule
+import com.example.noteapp.feature_note.presentation.MainActivity
+import com.example.noteapp.feature_note.presentation.util.Screen
+import com.example.noteapp.ui.theme.NoteAppTheme
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 
 @HiltAndroidTest
 @UninstallModules(AppModule::class)//hey dagger dont use this module.
 class NotesScreenTest{
+
+    //we can with this rule call inject fun before test case so we make sure it is correctly injected.
+    @get:Rule(order = 0) //smaller number will run before (ORDER).
+    val hiltRule = HiltAndroidRule(this)
+
+    //we need compose rule to make sure we run MainAcitivity and not some random activity.
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    //we can now simulate some clicks on specific icon
+    @Before
+    fun setUp(){
+        hiltRule.inject()
+        composeRule.setContent {
+            val navController = rememberNavController()
+            NoteAppTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.NotesScreen.route
+                ){
+                    composable(route = Screen.NotesScreen.route){
+                        NotesScreen(navController = navController)
+                    }
+                }
+            }
+        }
+    }
+
 
 }
